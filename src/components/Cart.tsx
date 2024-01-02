0801e51527a9ba20273673ab63239db3bccf0b02
shel7er <shell7er@gmail.com>
2024-01-03 01:38:48 +0700

    sending a sign-up verification email

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -73,7 +73,7 @@ const Cart = () => {
           <div className="flex h-full flex-col items-center justify-center space-y-1">
             <div className="relative mb-4 h-60 w-60 text-muted-foreground">
               <Image
-                src="/hippo-empty-cart.png"
+                src="/cat-empty-cart.png"
                 fill
                 alt="empty shopping cart hippo"
               />
